2897f98ebd21d27a50ada6d1289487d182c610c5
vladsaveliev <vladsaveliev@me.com>
2016-06-17 22:47:26 +0300

    Move logger and utils from Reporting Suite

--- logger.py
+++ logger.py
@@ -1,1 +1,205 @@
-../../reporting_suite/source/logger.py
+import getpass
+from os import environ
+import socket
+import sys
+from datetime import datetime
+from time import sleep
+import smtplib
+from email.mime.text import MIMEText
+import traceback
+
+
+log_fpath = None
+project_name = None
+project_fpath = None
+proc_name = None
+email_address = None
+is_debug = False
+
+smtp_host = None  # set up in source/config.py and system_info.yaml
+my_address = 'Vlad.Saveliev@astrazeneca.com'
+
+
+error_msgs = []
+warning_msgs = []
+critical_msgs = []
+
+
+def is_local():
+    hostname = socket.gethostname()
+    return 'local' in hostname or 'Home' in hostname or environ.get('PYTHONUNBUFFERED')
+
+
+def timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def step_greetings(name):
+    info()
+    info('-' * 70)
+    info(name)
+    info('-' * 70)
+
+
+def info(msg='', ending='\n', print_date=True, severity='info'):
+    _log(sys.stdout, msg, ending, print_date, severity=severity)
+
+
+def debug(msg='', ending='\n', print_date=True, severity='debug'):
+    if is_debug:
+        _log(sys.stdout, msg, ending, print_date, severity=severity)
+
+
+def warn(msg='', ending='\n', print_date=True, severity='warning'):
+    _log(sys.stderr, msg, ending, print_date, severity=severity)
+
+
+def silent_err(msg='', ending='\n', print_date=True, severity='silent_err'):
+    warn(msg, ending, print_date, severity=severity)
+
+
+def err(msg='', ending='\n', print_date=True, severity='error'):
+    warn(msg, ending, print_date, severity=severity)
+
+
+def send_email(msg_other='', subj='', only_me=False, email_by_prid=None):
+    if not msg_other or not smtp_host:
+        return
+
+    me_address = email_by_prid['klpf990'] if email_by_prid else None
+
+    prid = getpass.getuser()
+
+    other_address = None
+    if not only_me:
+        if email_address:
+            other_address = email_address
+        elif email_by_prid and prid in email_by_prid:
+            other_address = email_by_prid[prid]
+    if other_address == me_address:
+        other_address = None
+
+    if not other_address and not me_address:
+        return
+
+    msg_other += '\n'
+    msg_other += '\n'
+    msg_other += 'Ran by ' + prid + '\n'
+    msg_other += '\n'
+    if critical_msgs:
+        msg_other += 'Critical errors during the processing:\n'
+        for m in critical_msgs:
+            msg_other += '  ' + m + '\n'
+        msg_other += '\n'
+
+    if error_msgs:
+        if critical_msgs:
+            msg_other += 'Other e'
+        else:
+            msg_other += 'E'
+        msg_other += 'rrors during the processing:\n'
+        for m in error_msgs:
+            msg_other += '  ' + m + '\n'
+
+    msg_me = msg_other[:]
+    if warning_msgs:
+        msg_me += 'Warnings during the processing:\n'
+        for m in warning_msgs:
+            msg_me += '  ' + m + '\n'
+
+    if not subj:
+        subj = ''
+        if project_name:
+            subj += project_name
+        else:
+            subj += 'Reporting'
+        if proc_name:
+            subj += ' - ' + proc_name
+
+    msg_other = MIMEText(msg_other)
+    msg_me = MIMEText(msg_me)
+
+    msg_other['Subject'] = msg_me['Subject'] = subj
+
+    msg_other['From'] = msg_me['From'] = 'TargQC notification'  # 'klpf990@rask.usbod.astrazeneca.com'
+    msg_other['To'] = other_address
+    msg_me['To'] = me_address
+
+    def try_send(host, msg_):
+        s = smtplib.SMTP(host)
+        s.sendmail(msg_['From'], msg_['To'].split(','), msg_.as_string())
+        s.quit()
+        # info('Mail sent to ' + msg_['To'] + ' using ' + host)
+
+    def print_msg():
+        for line in msg_other.as_string().split('\n'):
+            print '   | ' + line
+        print ''
+
+    msgs = []
+    if other_address:
+        msgs.append(msg_other)
+    if me_address:
+        msgs.append(msg_me)
+    for msg in msgs:
+        try:
+            try_send(smtp_host, msg)
+        except:
+            warn('Could not send email using the sever "' + smtp_host + '" with exception: ')
+            warn('  ' + '; '.join(traceback.format_exception_only(sys.exc_type, sys.exc_value)))
+            if smtp_host != 'localhost':
+                warn('Trying "localhost" as a server...')
+                try:
+                    try_send('localhost', msg)
+                except:
+                    warn('Could not send email using the sever "localhost" with exception: ')
+                    warn('  ' + '; '.join(traceback.format_exception_only(sys.exc_type, sys.exc_value)))
+                    print_msg()
+            else:
+                print_msg()
+
+
+class CriticalError(Exception):
+    pass
+
+
+def critical(msg=''):
+    if isinstance(msg, basestring):
+        err(msg, severity='critical')
+    else:
+        if not msg:
+            return
+        for m in msg:
+            err(m, severity='critical')
+    raise CriticalError(msg)
+
+
+def _log(out, msg='', ending='\n', print_date=True, severity=None):
+    msg = str(msg)
+
+    if print_date:
+        msg = timestamp() + '  ' + msg
+
+    out.write(msg + ending)
+
+    if severity == 'critical':
+        critical_msgs.append(msg)
+    if severity == 'error':
+        error_msgs.append(msg)
+    if severity == 'warning':
+        warning_msgs.append(msg)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+    if is_local():
+        sleep(0.01)
+
+    if log_fpath:
+        try:
+            open(log_fpath, 'a').write(msg + ending)
+        except IOError:
+            sys.stderr.write('Logging: cannot append to ' + log_fpath + '\n')
+            try:
+                open(log_fpath, 'w').write(msg + '\n')
+            except IOError:
+                sys.stderr.write('Logging: cannot write to ' + log_fpath + '\n')
--- utils.py
+++ utils.py
@@ -1,1 +1,176 @@
-../../reporting_suite/source/utils.py
+import hashlib
+from os import environ
+import socket
+import re
+from collections import OrderedDict
+from os.path import join, basename, splitext
+
+from logger import info, critical, err
+from file_utils import file_exists, verify_file, file_transaction, adjust_path
+
+
+class OrderedDefaultDict(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        if not args:
+            self.default_factory = None
+        else:
+            if not (args[0] is None or callable(args[0])):
+                raise TypeError('first argument must be callable or None')
+            self.default_factory = args[0]
+            args = args[1:]
+        super(OrderedDefaultDict, self).__init__(*args, **kwargs)
+
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = default = self.default_factory()
+        return default
+
+    def __reduce__(self):  # optional, for pickle support
+        args = (self.default_factory,) if self.default_factory else ()
+        return self.__class__, args, None, None, self.iteritems()
+
+
+def _tryint(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+
+def _alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [_tryint(c) for c in re.split('([0-9]+)', s)]
+
+
+def human_sorted(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=_alphanum_key)
+    return l
+
+
+def get_chr_lengths_from_seq(seq_fpath):
+    chr_lengths = []
+
+    if seq_fpath.endswith('.fai'):
+        seq_fpath = splitext(seq_fpath)[0]
+
+    if verify_file(seq_fpath + '.fai', silent=True):
+        info('Reading genome index file (.fai) to get chromosome lengths')
+        with open(adjust_path(seq_fpath + '.fai'), 'r') as handle:
+            for line in handle:
+                line = line.strip()
+                if line:
+                    chrom, length = line.split()[0], line.split()[1]
+                    chr_lengths.append((chrom, length))
+    elif verify_file(seq_fpath, silent=True):
+        info('Reading genome sequence (.fa) to get chromosome lengths')
+        with open(adjust_path(seq_fpath), 'r') as handle:
+            from Bio import SeqIO
+            reference_records = SeqIO.parse(handle, 'fasta')
+            for record in reference_records:
+                chrom = record.id
+                chr_lengths.append((chrom, len(record.seq)))
+    else:
+        critical('Can\'t find ' + seq_fpath + ' and ' + seq_fpath + '.fai')
+    return chr_lengths
+
+
+def get_ext_tools_dirname(is_common_file=False):
+    from sys import platform as _platform
+    if not is_common_file and 'darwin' in _platform:
+        return join('ext_tools', 'osx')
+    else:
+        return 'ext_tools'
+
+
+def get_db_path(cnf, dbconf, dbname):
+    db_path = cnf['genome'].get(dbname)
+    if not db_path:
+        db_path = dbconf.get('path')
+        if not db_path:
+            err('Please, provide a path to ' + dbname + ' in the "genomes" section in the system config. The config is: ' + str(cnf['genome']))
+            return None
+    return verify_file(db_path, is_critical=True)
+
+
+def format_integer(name, value, unit=''):
+    value = int(value)
+    if value is not None:
+        return '{name}: {value:,}{unit}'.format(**locals())
+    else:
+        return '{name}: -'.format(**locals())
+
+
+def format_decimal(name, value, unit=''):
+    if value is not None:
+        return '{name}: {value:.2f}{unit}'.format(**locals())
+    else:
+        return '{name}: -'.format(**locals())
+
+
+def mean(values):
+    return float(sum(values)) / len(values) if len(values) > 0 else float('nan')
+
+
+def median(values):
+    values = sorted(values)
+
+    if len(values) % 2 == 1:  # odd number of values
+        return values[(len(values) - 1) / 2]
+    else:  # even number of values - take the avg of central
+        return (values[len(values) / 2] + values[len(values) / 2 - 1]) / 2
+
+
+def get_numeric_value(string_value):
+    """ parses string_value and returns only number-like part
+    """
+    num_chars = ['.', '+', '-']
+    number = ''
+    for c in string_value:
+        if c.isdigit() or c in num_chars:
+            number += c
+    return number
+
+
+def is_uk():
+    hostname = socket.gethostname()
+    return 'ukap' in hostname
+
+def is_china():
+    hostname = socket.gethostname()
+    return 'cniclhpc' in hostname
+
+def is_local():
+    hostname = socket.gethostname()
+    return 'local' in hostname or 'Home' in hostname or environ.get('PYTHONUNBUFFERED')
+
+def is_us():
+    hostname = socket.gethostname()
+    return any(name in hostname for name in ['rask', 'chara', 'blue', 'green', 'espo', 'orr', 'usbod', 'bn0'])
+
+def is_az():
+    return is_us() or is_uk() or is_china() or is_sweden()
+
+def is_cloud():
+    hostname = socket.gethostname()
+    return 'starcluster' in hostname
+
+def is_sweden():
+    hostname = socket.gethostname()
+    return 'seml' in hostname
+
+
+def md5(fpath):
+    hash = hashlib.md5()
+    with open(fpath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+
+def gray(text):
+    return '<span class="gray">' + text + '</span>'
