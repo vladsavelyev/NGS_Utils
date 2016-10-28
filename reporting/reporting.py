60b8d8afa54aa0e7fb68d068afe199e995b66135
vladsaveliev <vladsaveliev@me.com>
2016-10-28 14:33:41 +0300

    Fix checking islink in html assets

--- reporting/reporting.py
+++ reporting/reporting.py
@@ -1708,7 +1708,7 @@ def _embed_css_and_scripts(html, report_dirpath,
                 aux_dirpath = safe_mkdir(join(report_dirpath, aux_dirname))
                 fpath_in_aux = join(aux_dirpath, rel_fpath)
                 safe_mkdir(dirname(fpath_in_aux))
-                if not os.path.exists(fpath_in_aux):
+                if not os.path.exists(fpath_in_aux) and not os.path.islink(fpath_in_aux):
                     os.symlink(fpath, fpath_in_aux)
                 relpath_in_aux = relpath(fpath_in_aux, report_dirpath)
                 line_formatted = line.replace(rel_fpath, relpath_in_aux)
