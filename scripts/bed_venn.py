53063e5888e7c93846a121d837f95ac2ee4f8aff
vladsaveliev <vladsaveliev@me.com>
2017-01-25 22:43:07 +0300

    Update bed_venn.py (custom lables)

--- scripts/bed_venn.py
+++ scripts/bed_venn.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 import sys
 from optparse import OptionParser
 import subprocess
@@ -9,9 +9,6 @@ import itertools
 from ngs_utils.bed_utils import verify_bed
 from ngs_utils.file_utils import adjust_path, safe_mkdir
 from ngs_utils.logger import critical
-
-# beds_dirpath = '/Users/vlad/googledrive/az/reference_data/genomes/Hsapiens/hg19/bed/intersection/merged'
-# work_dirpath = '/Users/vlad/googledrive/az/reference_data/genomes/Hsapiens/hg19/bed/intersection/merged/intersections'
 
 
 def call(cmdl):
@@ -65,66 +62,80 @@ def calc_set_intersection(work_dirpath, sorted_beds_subset, intersection_bed_by_
     return intersection_bed
 
 
-def save_venn_diagram_data(size_by_set, label_by_set):
+def save_venn_diagram_data(size_by_set, names_map):
     data = []
     for venn_set, size in size_by_set.iteritems():
         set_info = dict()
         set_info['size'] = size
-        if isinstance(venn_set, tuple):
-            set_info['sets'] = list(venn_set)
-        else:
-            set_info['sets'] = [venn_set]
-        if isinstance(venn_set, int):
-            set_info['label'] = label_by_set[venn_set]
+        # if isinstance(venn_set, tuple):
+        set_info['sets'] = [names_map.get(n, n) for n in venn_set]
+        # else:
+        #     set_info['sets'] = [venn_set]
+        # if isinstance(venn_set, int):
+        #     set_info['label'] = label_by_set[venn_set]
         data.append(set_info)
     return json.dumps(sorted(data, key=lambda x: x['sets']))
 
 
-def main(bed_fpaths, output_dir):
-    # beds = tuple([join(beds_dirpath, bed) for bed in [
-    #     'AZ.bed',
-    #     # 'CRE.bed',
-    #     # 'IDT.bed',
-    #     # 'Med.bed',
-    #     'One.bed',
-    #     'One.hg38tohg19.bed',
-    #     # 'V6.bed',
-    # ]])
-
-    safe_mkdir(output_dir)
-    work_dirpath = safe_mkdir(join(output_dir))
-
+def run(work_dirpath, bed_fpaths):
     intersection_bed_by_subset = dict()
     calc_set_intersection(work_dirpath, sorted(bed_fpaths), intersection_bed_by_subset)
 
     intersection_size_by_subset = dict()
-    label_by_subset = dict()
+    # label_by_subset = dict()
     for bed_set, intersection_bed in intersection_bed_by_subset.items():
         bed_set = tuple([splitext(basename(b))[0] for b in bed_set])
         intersection_size_by_subset[bed_set] = bedsize(intersection_bed)
-        label_by_subset[bed_set] = basename(splitext(intersection_bed)[0])
+        # label_by_subset[bed_set] = basename(splitext(intersection_bed)[0])
         print (str(bed_set) + ': ' + basename(intersection_bed) + ', size: ' + str(intersection_size_by_subset[bed_set]))
+    return intersection_size_by_subset
 
-    json_txt = save_venn_diagram_data(intersection_size_by_subset, label_by_subset)
+
+beds_dirpath = '/Users/vlad/googledrive/az/reference_data/genomes/Hsapiens/hg19/bed/intersection/merged'
+
+
+def main():
+    parser = OptionParser(usage='Usage: ' + basename(__file__) + ' bed1 bed2 ... -o results_dir')
+    parser.add_option('-o', '--output-dir', dest='output_dir')
+    (opts, args) = parser.parse_args(sys.argv[1:])
+
+    if len(args) < 1:
+        bed_fpaths = tuple([join(beds_dirpath, bed) for bed in [
+            'AZ.bed',
+            'CRE.bed',
+            'IDT.bed',
+            'Med.bed',
+            # 'One.bed',
+            # 'One.hg38tohg19.bed',
+            'V6.bed',
+        ]])
+        names_map = {
+            'AZ': 'AZ Exome',
+            'CRE': 'B',
+            'V6': 'A',
+            'IDT': 'D',
+            'Med': 'C'}
+    else:
+        bed_fpaths = [verify_bed(bed) for bed in args]
+        names_map = dict()
+
+    if not opts.output_fpath:
+        output_dir = beds_dirpath
+    else:
+        output_dir = adjust_path(opts.output_dir)
+        safe_mkdir(output_dir)
+    work_dirpath = safe_mkdir(join(output_dir, 'intersections'))
+
+    intersection_size_by_subset = run(work_dirpath, bed_fpaths)
+
+    json_txt = save_venn_diagram_data(intersection_size_by_subset, names_map)
     print json_txt
 
     # TODO: generate HTML
 
 
 if __name__ == '__main__':
-    parser = OptionParser(usage='Usage: ' + basename(__file__) + ' bed1 bed2 ... -o results_dir')
-    parser.add_option('-o', '--output-dir', dest='output_dir')
-    (opts, args) = parser.parse_args(sys.argv[1:])
-
-    if len(args) < 1:
-        critical(parser.usage)
-    if not opts.output_fpath:
-        critical(parser.usage)
-
-    bed_fpaths = [verify_bed(bed) for bed in args]
-    output_dir = adjust_path(opts.output_dir)
-
-    main(bed_fpaths, output_dir)
+    main()
 
 
 # sets = []
