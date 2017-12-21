059c3aeac24d99f2b7f442b1d121bcbb07b408ce
vladsaveliev <vladislav.sav@gmail.com>
2017-12-21 11:15:30 +1100

    vcf_utils: get tumor/normal sample IDs and names from VCF

--- ngs_utils/vcf_utils.py
+++ ngs_utils/vcf_utils.py
@@ -2,51 +2,65 @@ import re
 from ngs_utils.file_utils import open_gzipsafe
 
 
+def get_sample_names(vcf_path):
+    return get_sample_ids(vcf_path)
+
+
 def get_tumor_sample_name(vcf_path):
-    return _get_sample_id(vcf_path, 'Tumor', return_name=True)
+    return get_sample_ids(vcf_path, return_names=True)[0]
 
 
 def get_normal_sample_name(vcf_path):
-    return _get_sample_id(vcf_path, 'Germline', return_name=True)
+    return get_sample_ids(vcf_path, return_names=True)[1]
 
 
 def get_tumor_sample_id(vcf_path):
-    return _get_sample_id(vcf_path, 'Tumor')
+    return get_sample_ids(vcf_path)[0]
 
 
 def get_normal_sample_id(vcf_path):
-    return _get_sample_id(vcf_path, 'Germline')
+    return get_sample_ids(vcf_path)[1]
 
 
-def _get_sample_id(vcf_path, key='Tumor', return_name=False):
-    """ Finds tumor or control sample name/id from a bcbio-derived VCF.
+def get_sample_ids(vcf_path, return_names=False):
+    """ Finds tumor and control sample names/ids from a bcbio-derived VCF,
+        and returns a tuple (tumor, control)
+
         1. If ##SAMPLE header field is found, use that for the name.
-        2. Otherwise, return the first (for tumor) or second (for normal)
-        sample in #CHROM header.
+        2. Otherwise, return the first (for tumor) and second (for normal)
+           sample in #CHROM header.
     """
-    assert key in ['Tumor', 'Germline']
-    sample_name = None
+    tumor_name, control_name = None, None
+    tumor_id, control_id = None, None
+
     with open_gzipsafe(vcf_path) as f:
         for line in f:
-            m = re.match(r'^##SAMPLE=<ID=(?P<name>\S+),Genomes=' + key + '>$', line)
+            m = re.match(r'^##SAMPLE=<ID=(?P<name>\S+),Genomes=Tumor>$', line)
             if m:
-                sample_name = m.group('name')
-                if return_name:
-                    return sample_name
+                tumor_name = m.group('name')
+            m = re.match(r'^##SAMPLE=<ID=(?P<name>\S+),Genomes=Germline>$', line)
+            if m:
+                control_name = m.group('name')
+
+            if tumor_name and return_names:
+                return tumor_name, control_name
 
             if line.startswith('#CHROM'):
                 samples = line.strip().split('\t')[9:]
-                if sample_name:
-                    sample_id = samples.index(sample_name)
+                if tumor_name:
+                    tumor_id = samples.index(tumor_name)
+                    if control_name:
+                        control_id = samples.index(control_name)
                 else:
-                    if key == 'Tumor':
-                        sample_id = 0
-                    else:
-                        sample_id = 1
+                    tumor_id, control_id = 0, 1
+                    tumor_name = samples[tumor_id]
+                    if len(samples) > 1:
+                        control_name = samples[control_id]
 
-                if return_name:
-                    return samples[sample_id]
+                if return_names:
+                    return tumor_name, control_name
                 else:
-                    return sample_id
+                    return tumor_id, control_id
     raise ValueError
+
 
