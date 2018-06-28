20ab4179086e8431f507afb992871875ad427aa3
vladsaveliev <vladislav.sav@gmail.com>
2018-06-28 20:23:22 +1000

    Add snakemake_utils for running snakemake on clusters

---
+++ ngs_utils/snakemake_utils/__init__.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+from os.path import dirname, abspath, join
+
+from ngs_utils import logger
+from ngs_utils.file_utils import safe_mkdir
+
+
+def package_path():
+    return dirname(abspath(__file__))
+
+
+def get_submit_script():
+    return join(package_path(), 'submit.py')
+
+
+def make_cluster_cmdl(log_dir):
+    from python_utils.hpc import get_loc
+    loc = get_loc()
+    if not loc.cluster:
+        logger.critical(f'Automatic cluster submission is not supported for the machine "{loc.name}"')
+
+    cluster_submitter = get_submit_script()
+    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    cluster_cmdl = f' --cluster "python {cluster_submitter} {timestamp} {log_dir} umccrise"'
+
+    # Also overriding jobscript?
+    jobscript = loc.cluster.get('jobscript')
+    if jobscript:
+        fixed_jobscript = join(log_dir, 'jobscript.sh')
+        with open(jobscript) as f_in, open(fixed_jobscript, 'w') as f_out:
+            f_out.write(f_in.read().replace('{path}', os.environ["PATH"]))
+        cluster_cmdl += f' --jobscript "{fixed_jobscript}"'
+
+    return cluster_cmdl
---
+++ ngs_utils/snakemake_utils/submit.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from python_utils.hpc import get_loc
+from snakemake.utils import read_job_properties
+
+
+# Read arguments
+args = sys.argv[1:]
+timestamp = args[0]
+logs_dir = args[1]
+application_name = args[2] if len(args) >= 4 else 'snakemake'
+job_cmd = sys.argv[-1]
+
+# Get submit command from python_utils/hpc.py
+loc = get_loc()
+submit_cmd = loc.cluster['submit_cmd']
+
+# Replace submit command wildcards with current job properties
+job_properties = read_job_properties(job_cmd)
+
+submit_cmd = submit_cmd\
+    .replace('{threads}', str(job_properties.get('threads', 1)))\
+    .replace('{resources.mem_mb}', str(job_properties.get('resources', {}).get('mem_mb', 2000)))
+
+job_name = job_properties.get('rule') or job_properties.get('groupid') or application_name
+if 'wildcards' in job_properties:
+    job_name += '.' + '__'.join(job_properties['wildcards'].values())
+
+submit_cmd = submit_cmd\
+    .replace('{job_name}', job_name)\
+    .replace('{log_file}', os.path.join(logs_dir, f'{timestamp}_{job_name}.cluster.log'))
+
+# Run submittion command
+cmd = f'{submit_cmd} {job_cmd}'
+sys.stderr.write(cmd + '\n')
+os.system(cmd)
