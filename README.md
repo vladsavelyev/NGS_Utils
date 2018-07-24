f24334347ca75a0a803e4fb5d34da474922bfeab
vladsaveliev <vladislav.sav@gmail.com>
2018-07-24 18:25:00 +1000

    Add ref files: key genes, supressors, predispose, Snakefile to generate key genes beds, option to fail if ref file not exist

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 [![Anaconda-Server Badge](https://anaconda.org/vladsaveliev/ngs_utils/badges/installer/conda.svg)](https://conda.anaconda.org/vladsaveliev)
 
 ## NGS Utils
-Helper code for NGS_Reporting and other bioinfomatics projects.
-Contains only lightweight source without any reference or test data, 
-so supposed to be small and can be safely used as a dependency 
-in other projects.
+
+Helper code for [AstraZeneca](https://github.com/AstraZeneca-NGS) and [UMCCR](https://github.com/umccr) bioinfomatics projects 
+(e.g. [NGS_Reporting](https://github.com/AstraZeneca-NGS/NGS_Reporting), [umccrise](https://github.com/umccr/umccrise)).
+
+Contains only lightweight source without any reference or test data, so supposed to be small and can be safely used as 
+a dependency in other projects.
 
 
 ### Installation
+
+From source:
+
+```
+python setup.py instasll
+```
+
+For development:
+
+```
+pip install -e .
+```
+
+Stable conda package:
+
 ```
 conda install -c vladsaveliev ngs_utils
 ```
--- ngs_utils/reference_data/__init__.py
+++ ngs_utils/reference_data/__init__.py
@@ -1,6 +1,7 @@
 from os.path import dirname, join, abspath, splitext, isfile
 
 from ngs_utils.file_utils import verify_file, adjust_path, verify_dir
+from ngs_utils.key_genes_utils import get_genes_from_file
 from ngs_utils.logger import critical, debug
 
 
@@ -11,7 +12,7 @@ def check_genome(genome):
     if genome not in SUPPORTED_GENOMES:
         critical('Genome ' + str(genome) + ' is not supported. Supported genomes: ' + ', '.join(SUPPORTED_GENOMES))
 
-def _get(relative_path, genome=None):
+def _get(relative_path, genome=None, is_critical=False):
     if genome:
         check_genome(genome)
     else:
@@ -19,13 +20,15 @@ def _get(relative_path, genome=None):
     relative_path = relative_path.format(genome=genome)
 
     path = abspath(join(dirname(__file__), relative_path))
+    if is_critical:
+        return verify_file(path, is_critical=True)
     return path
 
 
 ######################
 ######## FAI #########
-def get_fai(genome):
-    return _get(join('fai', '{genome}.fa.fai'), genome)
+def get_fai(genome, is_critical=False):
+    return _get(join('fai', '{genome}.fa.fai'), genome, is_critical=is_critical)
 
 def get_chrom_lengths(genome=None, fai_fpath=None):
     assert genome or fai_fpath, f'One of genome or fai_fpath should be not None: genome={genome}, fai_fpath={fai_fpath}'
@@ -65,8 +68,30 @@ def get_chrom_order(genome=None, fai_fpath=None):
     chr_order = {c: i for i, (c, l) in enumerate(chr_lengths)}
     return chr_order
 
-def ucsc_to_ensembl(genome):
+def ucsc_to_ensembl(genome, is_critical=False):
     """ mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -N -e "select * from ucscToEnsembl;" hg19 > hg19.ucscToEnsembl.tsv
         mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -N -e "select * from ucscToEnsembl;" hg38 > hg38.ucscToEnsembl.tsv
     """
-    return _get(join('fai', '{genome}.ucscToEnsembl.tsv'), genome)
+    return _get(join('fai', '{genome}.ucscToEnsembl.tsv'), genome, is_critical=is_critical)
+
+
+#############################
+######## Gene lists #########
+
+def get_signatures_probabilities(is_critical=False):
+    return _get('signatures_probabilities.txt', is_critical=is_critical)
+
+def get_suppressors(is_critical=False):
+    return _get('suppressors.txt', is_critical=is_critical)
+
+def get_cancer_genes_ensg(is_critical=False):
+    return _get('predisposition_genes_engs.txt', is_critical=is_critical)
+
+def get_key_genes_bed(genome, is_critical=False):
+    return _get('key_genes/key_genes.' + genome + '.bed', is_critical=is_critical)
+
+def get_key_genes_set(is_critical=False):
+    return \
+        get_genes_from_file(_get('key_genes/az_key_genes.300.txt', is_critical=is_critical)) | \
+        get_genes_from_file(_get('key_genes/umccr_extra_key_genes.txt', is_critical=is_critical))
+
