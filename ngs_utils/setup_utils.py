81f02cf37c841d4aa6e62194ab543a9a491d6fc8
vladsaveliev <vladislav.sav@gmail.com>
2018-11-03 10:34:39 +1100

    Release: push only once

--- ngs_utils/setup_utils.py
+++ ngs_utils/setup_utils.py
@@ -26,10 +26,9 @@ def release(new_version, package_name=None):
     '''
     version_file, new_version = increment_version(new_version, package_name)
     run_cmdl(f'git add {version_file}')
-    run_cmdl(f'git commit -m "Bump {new_version} [ci skip]"')
+    run_cmdl(f'git commit -m "Bump {new_version}"')
     run_cmdl(f'git tag {new_version}')
-    run_cmdl(f'git push')
-    run_cmdl(f'git push --tags')
+    run_cmdl(f'git push origin --tags')  # pushes both commits and tags
 
 
 def err(msg=''):
