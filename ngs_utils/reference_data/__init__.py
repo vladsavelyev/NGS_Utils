06310223e899ebfa167c0f75941936f4d4915c6b
vladsaveliev <vladislav.sav@gmail.com>
2020-04-20 22:54:28 +1000

    Generate predispose genes bed and add get_predispose_genes_(txt,bed) functions

--- ngs_utils/reference_data/__init__.py
+++ ngs_utils/reference_data/__init__.py
@@ -102,6 +102,13 @@ def get_key_genes_bed(genome, is_critical=False, coding_only=False):
     return _get(f'key_genes/umccr_cancer_genes.{genome}.{"transcript" if not coding_only else "coding"}.bed',
                 is_critical=is_critical)
 
+def get_predispose_genes_txt():
+    return _get('key_genes/sources/predispose_genes.txt')
+
+def get_predispose_genes_bed(genome):
+    return _get(f'key_genes/predispose_genes.{genome}.coding.bed')
+
+
 #############################
 ###### Known fusions ########
 
